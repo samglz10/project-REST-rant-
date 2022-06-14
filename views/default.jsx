c1f4c65466222077ba463d147721c3066ac5da26
Samuel Gonzalez <samglz10@gmail.com>
2022-06-13 22:41:04 -0400

    Adding default view for layout

---
+++ views/default.jsx
@@ -0,0 +1,13 @@
+const React = require('react')
+const Def = ((htmnl) =>{
+return (
+    <html>
+        <head>
+            <title>Title</title>
+        </head>
+        <body>
+            {html.children}
+        </body>
+    </html>
+)
+})
