4eb5d5643a7cd33bcd07f252883d416f331b5c09
Samuel Gonzalez <samglz10@gmail.com>
2022-06-25 21:34:18 -0400

    show page fixed?

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,10 +6,21 @@ const show = (data) => {
         <Def>
             <main>
                 <h1>{data.place.name}</h1> 
-                {/* How to view you data
+                {/* How to view your data
                 <pre>
                     {JSON.stringify(data,undefined,2)}
                 </pre>*/}
+                <h3>
+                    Located in {data.place.city}, {data.place.state}
+                </h3>
+                <h2>
+                    Description
+                </h2>
+                <h3>
+                    {data.place.showEstablished()}
+                </h3>
+                <h4>Serving {data.place.cuisines}
+                </h4>
                 <p>Rating</p>
                 <a href={`/places/${data.id}/edit`} className="btn btn-warning">
                     Edit
