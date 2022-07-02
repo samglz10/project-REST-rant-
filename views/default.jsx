7a474a76660628b743019fc6f5df9b33936c9aa3
Samuel Gonzalez <samglz10@gmail.com>
2022-07-02 14:16:17 -0400

    fixed GET path picking up styles.css as a path, updated styles.css as an absolute path

--- views/default.jsx
+++ views/default.jsx
@@ -6,7 +6,7 @@ return (
         <head>
             <title>Title</title>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"/>
-            <link rel="stylesheet" href="style.css"/>
+            <link rel="stylesheet" href="/style.css"/>
         </head>
         <body>
             <nav>
--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -51,21 +51,3 @@ const editForm = (data)=> {
 }
 
 module.exports = editForm
-
-<h1>Edit Place</h1>
-<form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
-    ...
-    <div className="row">
-        ...
-        <div className="form-group col-sm-4">
-            <label htmlFor="founded">Founded</label>
-            <input className="form-control" 
-              id="founded" 
-              name="founded" 
-              value={data.place.founded} 
-            />
-        </div>
-    </div>
-    ...
-    <input className="btn btn-primary" type="submit" value="Update Place" />
-</form>
