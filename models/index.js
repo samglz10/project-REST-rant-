5c9eaaaa239f924ee89dfeb9fd673f757db7efcf
Samuel Gonzalez <samglz10@gmail.com>
2022-10-14 17:36:26 -0400

    added ENV path

--- models/index.js
+++ models/index.js
@@ -1,8 +1,10 @@
 // Adds the dotenv package AKA MONGO_URI eviroment variable
-require('dotenv').config()
+require('dotenv').config({
+  path: 'ENV_FILENAME'
+})
 const mongoose = require('mongoose')
 //changed from mongoose.connect to 
-mongoose.createConnection(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, 
   useUnifiedTopology: true
 })
