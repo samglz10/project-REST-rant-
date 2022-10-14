15e30bad91f121c172235996c7983121c8f45f60
Samuel Gonzalez <samglz10@gmail.com>
2022-10-14 17:29:53 -0400

    moongoose.connect to mongoose.createConnection

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 // Adds the dotenv package AKA MONGO_URI eviroment variable
 require('dotenv').config()
 const mongoose = require('mongoose')
-
-mongoose.connect(process.env.MONGO_URI, {
+//changed from mongoose.connect to 
+mongoose.createConnection(process.env.MONGO_URI, {
   useNewUrlParser: true, 
   useUnifiedTopology: true
 })
