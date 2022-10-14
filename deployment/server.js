e80b7e2b8992cc1e29a3db00ff9f5a19e4271374
Samuel Gonzalez <samglz10@gmail.com>
2022-10-14 17:12:52 -0400

    first

--- deployment/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
-})
-
-app.listen(process.env.PORT)
