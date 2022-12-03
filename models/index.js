a5d85dd626f046ed20d2f98fc61738870278b96d
Samuel Gonzalez <samglz10@gmail.com>
2022-12-03 15:46:01 -0500

    env file names changed

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const mongoose = require('mongoose')
 //changed from mongoose.connect to 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true, 
   useUnifiedTopology: true
 })
